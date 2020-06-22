2478301c44db9462393f28e4d94453f2eac62347
abdul azeem <30392205+AbdulAzeem97@users.noreply.github.com>
2020-06-22 12:28:09 +0500

    Update App.js
    Checking with the errors

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { Message2 } from "./message2";
 import "./App.css";
 
 export default function App() {
-  let [count, setCount] = useState(1);
+  // let [count, setCount] = useState(1);
   // setCount(2);
-  let {isMorning, setMorning } = useState(false)
+  let { isMorning, setMorning } = useState(true);
 
   return (
-    <div >
-    {/* // className={`box ${isMorning ? "dayLight" : ""}`}>  */}
-      <h1> Day Time = {isMorning ? 'Night' : 'Night nai likhunga'}</h1>
-      <Message2 counter2={++count} />
-      <Message counter={count} />/<br></br>
-      <button onClick={() => setCount(++count)}> Counter Button </button>
-      <br></br>
-      <h2>Programming by Java Script</h2>;
+    <div>
+      {/* // className={`box ${isMorning ? "dayLight" : ""}`}>  */}
+      <h1> Day Time = {isMorning ? "Morning " : "Night"}</h1>
+      <button onClick={() => setMorning(!isMorning)}> Counter Button </button>
+      {/* <Message2 counter2={++count} />
+      <Message counter={count} />/<br></br> */}
+      {/* <button onClick={() => setCount(++count)}> Counter Button </button> */}
+      {/* <br></br>
+      <h2>Programming by Java Script</h2>; */}
     </div>
   );
 }
